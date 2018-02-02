e36fef44b8c1b2c7a81f8143747bf4a9d79192eb
Ali Abrar <aliabrar@gmail.com>
2018-02-02 14:55:53 -0500

    Add Show instances to Party and Tradeable

--- src/DecisionTree.hs
+++ src/DecisionTree.hs
@@ -36,7 +36,7 @@ data DecisionStep x = Done
   deriving Functor
 
 data Party = FirstParty | Counterparty | ThirdParty PartyName
-  deriving (Eq, Show)
+  deriving (Eq, Ord, Show)
 
 -- See also data TradeDir below
 
